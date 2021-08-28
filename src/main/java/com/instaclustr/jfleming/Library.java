19be9a92689c8abcb9e5129f53062c50bf7f0d8e
jacko161 <jacksonrobertfleming161@gmail.com>
2021-08-28 19:47:21 +1000

    Initial Commit

---
+++ src/main/java/com/instaclustr/jfleming/Library.java
@@ -0,0 +1,17 @@
+package com.instaclustr.jfleming;
+
+/**
+ * Dev week 2021 - Library Project
+ * @author - Jackson Fleming (jackson.fleming@instaclustr.com)
+ * @version 0.0.1
+ * @since - 2021-08-24
+ */
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+    public boolean isStringEmptyOrNull(String input){
+        return input == null || input.isEmpty();
+    }
+}
